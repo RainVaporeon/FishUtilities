c1a941fe38eff8947d3f15a8b6819db215efd540
Ori <38721999+OriSpirit@users.noreply.github.com>
2023-09-19 23:08:01 +0800

    WeightedRandom updating

--- src/main/java/com/spiritlight/fishutils/Main.java
+++ src/main/java/com/spiritlight/fishutils/Main.java
@@ -1,21 +1,70 @@
 package com.spiritlight.fishutils;
 
-import com.spiritlight.fishutils.tests.Test;
-import sun.misc.Unsafe;
+import com.spiritlight.fishutils.misc.functions.Predicates;
+import com.spiritlight.fishutils.random.WeightedRandom;
+import com.spiritlight.fishutils.utils.SpliteratorSupport;
+import com.spiritlight.fishutils.utils.secure.SeededGenerator;
 
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.List;
+import java.util.Spliterator;
 import java.util.function.Consumer;
-import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
+    interface Callable {
+        boolean get();
+    }
 
+    private static Callable retrieve(boolean value) {
+        return () -> value;
+    }
+
+    public static void main(String[] args) {
+
+        // 1M calls of gathered together: 10026 (0.10026%)
+        // 1M calls of split up: 10063 (0.10063%)
+        // Difference is minimal and several iterations can minify the odds
+        try {
+            WeightedRandom<Callable> callers = new WeightedRandom<>();
+            WeightedRandom<Boolean> generator = new WeightedRandom<>();
+            int count = 0;
+            generator.addObject(false, 9900);
+            generator.addObject(true, 100);
+            for(int i = 0; i < 10000; i++) {
+                if(count >= 100) {
+                    callers.addObject(retrieve(false), 1);
+                } else {
+                    boolean val = generator.getNext();
+                    if(val) {
+                        callers.addObject(retrieve(true), 1);
+                        count++;
+                    } else {
+                        callers.addObject(retrieve(false), 1);
+                    }
+                }
+            }
+            System.out.println("Size count: " + callers.getOutcomes().size());
+            long winners = callers.infinitelySupplyingStream()
+                    .limit(1000000)
+                    .parallel()
+                    .map(Callable::get)
+                    .filter(Predicates.IDENTITY)
+                    .count();
+            System.out.println(winners);
         } catch (Throwable t) {
             //noinspection CallToPrintStackTrace
             t.printStackTrace();
         }
+    }
+
+    private static void printElements(Spliterator<?> sp) {
+        sp.forEachRemaining(System.out::println);
+    }
+
+
+    private static void printSeed(SeededGenerator gen) {
+        System.out.println("SEED=" + gen.getSeed());
+        System.out.println("VALUE30=" + gen.generate(30));
     }
 
     private static void populateArray(Object[][][] arr) {
--- src/main/java/com/spiritlight/fishutils/random/WeightedRandom.java
+++ src/main/java/com/spiritlight/fishutils/random/WeightedRandom.java
@@ -1,15 +1,46 @@
 package com.spiritlight.fishutils.random;
 
-import java.util.*;
+import com.spiritlight.fishutils.utils.Numbers;
+import com.spiritlight.fishutils.utils.enums.Secure;
 
+import java.security.SecureRandom;
+import java.util.*;
+import java.util.stream.Stream;
+
+/**
+ * A weighted random, this type of random is synchronized, meaning
+ * it may be used across threads safely without throwing an exception.
+ * <p>
+ *     The underlying random instance always uses a SecureRandom unless
+ *     the {@link com.spiritlight.fishutils.utils.enums.Secure} parameter is NO.
+ * @param <T>
+ */
 public class WeightedRandom<T> {
     private final Map<T, Integer> weightMap = new HashMap<>();
-    private final Random RANDOM = new Random(this.nextSalt(System.currentTimeMillis()));
+    private final Random RANDOM;
+
+    public WeightedRandom() {
+        this(Secure.YES);
+    }
+
+    public WeightedRandom(Secure secure) {
+        if(secure == Secure.NO) {
+            RANDOM = new Random();
+        } else {
+            byte[] random = new byte[256];
+            Numbers.nextBytes(random);
+            RANDOM = new SecureRandom(random);
+        }
+    }
 
     private int totalWeight = 0;
 
     private final Object mutex = new Object();
 
+    /**
+     * Generates the next element
+     * @return the element
+     */
     public T getNext() {
         synchronized (mutex) {
             int value = RANDOM.nextInt(totalWeight) + 1;
@@ -20,27 +51,71 @@ public class WeightedRandom<T> {
                 }
             }
         }
-        throw new IllegalStateException("totalWeight is larger than all items.");
+        throw new InternalError("totalWeight is larger than all items.");
     }
 
+    /**
+     * Retrieves all possible outcomes
+     * @return
+     */
+    public Set<T> getOutcomes() {
+        return Set.copyOf(weightMap.keySet());
+    }
 
+    /**
+     * Adds a new object into the generator
+     * @param object The object, it should not be in the list already.
+     * @param weight The weight of the object, should be higher than 0
+     */
     public void addObject(T object, int weight) {
         if(weight < 0) throw new IllegalArgumentException("weight cannot be less than 0");
-        weightMap.put(object, weight);
 
         synchronized (mutex) {
+            weightMap.put(object, weight);
             updateWeight();
         }
     }
 
+    /**
+     * Updates the object with a new weight
+     * @param object the object
+     * @param weight the weight of the object, should be higher than 0
+     * @apiNote This call is equal to calling {@link WeightedRandom#removeObject(Object)}
+     * and then call {@link WeightedRandom#addObject(Object, int)}
+     */
+    public void updateObject(T object, int weight) {
+        removeObject(object);
+        addObject(object, weight);
+    }
+
+    /**
+     * Removes this object from the random
+     * @param object the object
+     */
     public void removeObject(T object) {
-        weightMap.remove(object);
-
         synchronized (mutex) {
+            weightMap.remove(object);
             updateWeight();
         }
     }
 
+    /**
+     * Retrieves a stream source of this generator, the
+     * underlying stream is infinitely-supplying, and
+     * structure modifications will have an impact
+     * on the generated object.
+     * @return an infinitely supplying stream which calls {@link WeightedRandom#getNext()}
+     */
+    public Stream<T> infinitelySupplyingStream() {
+        return Stream.generate(this::getNext);
+    }
+
+    /**
+     * Retrieves the probability (in double) of getting this value in a roll
+     * @param object the object
+     * @return 0 if it is absent or is configured to have no odds of generating,
+     * otherwise a number between 0.0 to 1.0 denoting the odds of rolling this element.
+     */
     public double getProbabilityOf(T object) {
         if(!weightMap.containsKey(object)) return 0;
         return (double) weightMap.get(object) / totalWeight;
@@ -48,12 +123,5 @@ public class WeightedRandom<T> {
 
     private void updateWeight() {
         totalWeight = weightMap.values().stream().mapToInt(Integer::intValue).sum();
-    }
-
-    private long nextSalt(long in) {
-        long v1 = in * in >>> 2;
-        long v2 = v1 << 1 + 1;
-        long v3 = (int) Math.log(v2) * v2;
-        return v3 << 2 + in ^ Objects.hash(in, v1, v2, v3);
     }
 }
