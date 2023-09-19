621a40dba244622e7cc41413934ddabade98d8e0
Ori <38721999+OriSpirit@users.noreply.github.com>
2023-09-19 23:09:37 +0800

    add an unchecked utility method for ThrowingSupplier

--- src/main/java/com/spiritlight/fishutils/misc/ThrowingSupplier.java
+++ src/main/java/com/spiritlight/fishutils/misc/ThrowingSupplier.java
@@ -2,4 +2,12 @@ package com.spiritlight.fishutils.misc;
 
 public interface ThrowingSupplier<T> {
     T get() throws Throwable;
+    
+    default T getUnchecked() {
+        try {
+            return get();
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        }
+    }
 }
